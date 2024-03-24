cf384c7e37440bd429a4d5f04cdbb6f35649bb93
Natali Chervonnykh <Natali.millikka@gmail.com>
2024-03-24 19:02:08 +0200

    виправлена помилка задачі 3

--- js/task-3.js
+++ js/task-3.js
@@ -2,15 +2,12 @@
 
 const textInput = document.querySelector('#name-input');
 const textOutput = document.querySelector('#name-output');
-// const textStart = textOutput.textContent;
 
 textInput.addEventListener('input', event => {
-  // event.currentTarget.value.trim() === '';
   if (event.currentTarget.value.trim() === '') {
     textOutput.textContent = 'Anonymous';
     // return;
   } else {
     textOutput.textContent = event.currentTarget.value.trim();
   }
-  textOutput.textContent = textStart;
 });
